4fa0a0f7faef3525ddb575a05993088583055ce3
Revanth.H.S <cooldude.revanth@gmail.com>
2019-09-01 14:06:20 +0530

    Add files via upload

---
+++ followPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform follow;
+    public Vector3 offset;
+
+
+    void Update()
+    {
+
+        transform.position = follow.position + offset;
+    }
+}
